460fe1c8bd9ae06456e664c79dc4f38e036d0892
Mohith Shrivastava <mohith@codescience.com>
2018-06-17 13:45:39 -0400

    latest working code

--- chapter10/ltngOutApp/app.js
+++ chapter10/ltngOutApp/app.js
@@ -1,21 +1,24 @@
-import {OAuth, DataService} from 'forcejs';
+import {OAuth} from 'forcejs';
 
-let oauth = OAuth.createInstance();
+
+let oauth = OAuth.createInstance("3MVG9JamK_x9K2XI99fdzq3yLlbGhfhg6FEM_8bszwi4XeG1ktlVrQf1B9tZr8cuTLBgAkl2lk.psKz7JZw0Q"
+                                ,"https://test.salesforce.com",
+                                "https://infinite-brushlands-64356.herokuapp.com/oauthcallback.html"
+                                );
 oauth.login()
     .then(oauthResult => {
-        DataService.createInstance(oauthResult);
         console.log(oauthResult);
         renderLightningOutComponent(oauthResult);
     });
 
 let renderLightningOutComponent = (oauthResult) => {
-    const token = "Bearer "+ oauthResult.accessToken;
-    const lightningEndPointURI = "https://ability-page-3410-dev-ed..lightning.force.com";
-    $Lightning.use("c:youtubesearchOutApp", function() {
+    const token = oauthResult.accessToken;
+    const lightningEndPointURI = "https://ability-page-3410-dev-ed.lightning.force.com";
+    $Lightning.use("c:youtubesearchOutApp", () => {
         $Lightning.createComponent("c:youtubesearch", {},
            "youtubeApp",
-            function(cmp) {
-               // do some stuff
+            (cmp) => {
+               //Component COde
             });
   },lightningEndPointURI,token );
 
