576d975abf061b3677d6d1ac8081187d807469ca
Mohith Shrivastava <mohith@codescience.com>
2018-06-24 18:31:00 -0400

    code changes

--- chapter11/force-app/main/default/aura/LeadVerify/LeadVerifyController.js
+++ chapter11/force-app/main/default/aura/LeadVerify/LeadVerifyController.js
@@ -1,5 +1,13 @@
 ({
     init : function(component, event, helper) {
         helper.validateEmailService(component, event);
+    },
+
+    handleClick : function(component, event, helper) {
+        // Figure out which action was called
+        var actionClicked = event.getSource().getLocalId();
+        // Fire that action
+         var navigate = component.get('v.navigateFlow');
+        navigate(actionClicked);
     }
 })
